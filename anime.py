4558c73707860324e0e912b50065f0fcc0d538c6
XZDXRZ <13918165843@163.com>
2020-03-19 09:23:19 +0800

    Add some explanation and remove useless code

--- anime.py
+++ anime.py
@@ -5,69 +5,40 @@ from selenium import webdriver
 
 def get_page(key_word,page,t):
 
-    # 进入浏览器设置
+    # 浏览器设置
     options = webdriver.ChromeOptions()
-    # 设置中文
-    #options.add_argument('lang=zh_CN.UTF-8')
-    # 更换头部
     options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36"')
-    options.add_experimental_option('excludeSwitches', ['enable-automation'])
     driver = webdriver.Chrome(chrome_options=options)
 
-    #driver = webdriver.Chrome()
     driver.get("https://share.acgnx.se/search.php?sort_id=0&keyword="+key_word+"&page="+str(page))
 
-
-    #print(driver.page_source)
+    # 缓冲t秒
     sleep(t)
 
+    # 返回网页源代码
     return driver.page_source
 
 def catch(key_word,time=15,output_name='output_file'):
 
-    #f=open(file_name+'.html','r',encoding='utf-8')
-    #page = f.read()
-    #f.close()
-    #print(page)
+    cnt=1 # 总链接个数
+    pid=1 # 总页数
 
-    cnt=1
-    pid=1
-
+    # 清空输出文件
     f=open(output_name+'.txt','w',encoding='utf-8')
     f.write('')
     f.close()
 
     while True:
-
         page = get_page(key_word,pid,time)
-
         page.encode('utf-8')
 
         f=open(output_name+'.txt','a',encoding='utf-8')
 
         soup = BeautifulSoup(page,'lxml')
-        #soup.p.encode("utf-8")
-        #print(soup.prettify())
-        #print(soup.find_all('tr'))
-        k=1
-        #print(soup.find_all(style="text-align:left;")[0].get_text())
+        k=1 # 当前页的循环变量
         name = soup.find_all(style="text-align:left;")
+        # 查找链接
         for i in soup.find_all(id='magnet'):
-            '''
-            try:
-                #print(str(k)+' '+i['href'])
-                #print(name[k-1].get_text())
-                f.write('['+str(cnt)+']\n')
-                namelen=len(name[k-1].get_text())
-                f.write(name[k-1].get_text()[2:namelen-2]+'\n')
-                f.write(i['href']+'\n\n')
-            except TypeError:
-                print('TypeError!!!')
-                continue
-            except AttributeError:
-                print('AttributeError!!!')
-                continue
-            '''
             f.write('['+str(cnt)+']\n')
             namelen=len(name[k-1].get_text())
             f.write(name[k-1].get_text()[2:namelen-2]+'\n')
@@ -75,11 +46,12 @@ def catch(key_word,time=15,output_name='output_file'):
             k+=1
             cnt+=1
         f.close()
-        #print(len(soup.find_all('a','nextprev')))
+        # 只有一页的情况
         if len(soup.find_all('a','nextprev')) == 0:
             print("Page "+str(pid)+" OK!")
             #print('Just one Page.')
             break
+        # 只有两页，且当前是最后一页的情况
         if len(soup.find_all('a','nextprev')) == 1 and pid != 1:
             print("Page "+str(pid)+" OK!")
             #print('The last page.')
